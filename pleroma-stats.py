78e5bc02c697fadf8bdacb683aa121e9414575bf
Matthew Jorgensen <matthew@jrgnsn.net>
2020-10-16 16:27:31 -0500

    Implement local post count better

--- pleroma-stats.py
+++ pleroma-stats.py
@@ -52,7 +52,6 @@ pleroma_db = get_parameter("pleroma_db", config_filepath) # E.g., pleroma_prod
 pleroma_db_user = get_parameter("pleroma_db_user", config_filepath) # E.g., pleroma
 pleroma_db_password = get_parameter("pleroma_db_pass", config_filepath)
 grafana_db = get_parameter("grafana_db", config_filepath) # E.g., grafana_prod
-instance_name = get_parameter("instance_name", config_filepath) # E.g., example.com 
 
 # Postgres connection strings
 cstring_grafana = "dbname=" + grafana_db + " user=" + pleroma_db_user + " password=" + pleroma_db_password + " host='localhost' port='5432'"
@@ -128,7 +127,7 @@ try:
       num_servers = cur.fetchone()[0]
 
       # get status_count from Pleroma's DB
-      cur.execute("SELECT SUM(unlisted + public + private + direct) FROM counter_cache WHERE instance = " + instance_name + ";")
+      cur.execute("SELECT SUM(note_count) FROM users WHERE local = 't';")
       num_posts = cur.fetchone()[0]
 
       #############################################################################################
